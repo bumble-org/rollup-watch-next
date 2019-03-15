da4a2eaebe03234ebd0d90e2c9b3f04707b275a1
Jack Steam <jacksteamdev@gmail.com>
2019-03-15 11:28:52 -0500

    Refactor event handler

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import nanoid from 'nanoid'
 
 export default function watchNext(config, cb) {
   const watcher = watch(config)
+  if (cb) watcher.on('event', cb)
+
+  const id = nanoid()
+  watcher.id = id
+  process.emit('rollup-watch:start', id)
 
   const resolves = {
     START: [],
@@ -13,43 +18,77 @@ export default function watchNext(config, cb) {
     ERROR: [],
   }
   const rejects = []
-
-  const eventHandler = event => {
-    const tuple = { value: event, done: event.code === 'FATAL' }
-
-    resolves[event.code].forEach(fn => fn(tuple))
-    resolves[event.code] = []
-
-    if (tuple.done) {
-      rejects.forEach(fn => fn(tuple))
-      watcher.close()
-    }
-  }
-
-  if (cb) watcher.on('event', cb)
-  watcher.on('event', eventHandler)
-
-  watcher.next = code =>
-    new Promise((resolve, reject) => {
-      resolves[code].push(resolve)
-      rejects.push(reject)
-    })
-
-  const id = nanoid()
-  watcher.id = id
+  let done = false
 
   const closeWatcher = watcher.close
-  let closed = false
   watcher.close = () => {
-    if (!closed) {
+    if (!done) {
       process.emit('rollup-watch:close', id)
-      closed = true
+      eventHandler({ code: 'CLOSE' })
     }
 
     return closeWatcher()
   }
 
-  process.emit('rollup-watch:start', id)
+  const eventHandler = ({ code, ...value }) => {
+    // do not resurrect if done
+    done = done || ['FATAL', 'CLOSE'].includes(code)
+
+    // do not include event code in value
+    const tuple = { value, done }
+
+    if (code === 'CLOSE') {
+      // resolve all events with { done }
+      // on watcher.close()
+      Object.values(resolves)
+        .flat()
+        .forEach(fn => fn({ done }))
+    } else {
+      // if event is from watcher
+      // - resolves for watcher.next('FATAL')
+      resolves[code].forEach(fn => fn(tuple))
+      resolves[code] = []
+    }
+
+    if (code === 'FATAL') {
+      // should only reject on FATAL
+      // - will not reject for watcher.next('FATAL'),
+      //   since those promises have already resolved
+      rejects.forEach(fn => fn(tuple))
+      // make sure watcher is closed
+      closeWatcher()
+    }
+  }
+
+  watcher.on('event', eventHandler)
+
+  watcher.next = nextCode => {
+    if (!resolves[nextCode]) {
+      // cannot register invalid event code
+      throw new TypeError(
+        `${nextCode} is not a valid event code`,
+      )
+    }
+
+    return done
+      ? // if already done, resolve immediately
+        Promise.resolve({ done })
+      : // if not done
+        new Promise((resolve, reject) => {
+          if (nextCode) {
+            // if code is defined, resolve for that event
+            resolves[nextCode].push(resolve)
+          } else {
+            // if code is undefined, resolve for any event
+            Object.values(resolves).forEach(rs =>
+              rs.push(resolve),
+            )
+          }
+
+          // add to rejects regardless
+          rejects.push(reject)
+        })
+  }
 
   return watcher
 }
